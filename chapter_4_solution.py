ce323b2bc10c9422aba10c23beca681e4237d8c8
Pratham Doshi <pratham.doshi95@gmail.com>
2020-04-24 20:22:48 -0700

    chapter 4 questions set up

---
+++ chapter_4_solution.py
@@ -0,0 +1,32 @@
+# chapter 4 exercise solutions
+#--------------------------------------------------------------------------#
+
+# questions 1: Write a function that takes a number as an input and returns the number squared.
+print('\nQuestion 1 Solution: \n')
+print('*'*50)
+
+#--------------------------------------------------------------------------#
+
+# questions 2: Write a function that accepts a string as a parameter and prints it.
+print('\nQuestion 2 Solution: \n')
+print('*'*50)
+
+#--------------------------------------------------------------------------#
+
+# questions 3: Write a function that accepts three required params and two optional params. 
+print('\nQuestion 3 Solution: \n')
+print('*'*50)
+
+#--------------------------------------------------------------------------#
+
+# questions 4: Two functions: 1) Int as param and return divided it by two. 2) Take return from 1 and return * 4.
+print('\nQuestion 4 Solution: \n')
+print('*'*50)
+
+#--------------------------------------------------------------------------#
+
+# questions 5: Write a function that converts a string to float and add error handler. 
+print('\nQuestion 5 Solution: \n')
+print('*'*50)
+
+#--------------------------------------------------------------------------#
