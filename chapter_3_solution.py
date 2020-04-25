45612d1cfd13d11f8a1cb139f497082bd24bbe04
Pratham Doshi <pratham.doshi95@gmail.com>
2020-04-24 19:05:10 -0700

    Chapter 3 questions set! Using 1 pomodoro to answer all. T-25 till the next commit.

---
+++ chapter_3_solution.py
@@ -0,0 +1,13 @@
+# chapter 3 excecise solutions
+
+# questions 1: Print three different strings 
+
+# question 2: Write a program that prints if the number is less than 10 or greater than/equal to 10
+
+# question 3: Print something for number <=10, >10 or <=25, >25
+
+# question 4: Create a program that divides two variable and prints the remainder 
+
+# question 5: Create a program that divides two variables, divides them and prints the quotient
+
+# question 6: Write a program that prints different strings for different range of ages. 
