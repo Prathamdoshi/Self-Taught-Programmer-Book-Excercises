1d62d65edd3dc4081c11658971215a155fc30b14
Pratham Doshi <pratham.doshi95@gmail.com>
2020-04-26 02:19:51 -0700

    chapter 8 solution done and tested

---
+++ chapter_8_solution/cubed.py
@@ -0,0 +1,10 @@
+def cube(num):
+    """
+    Returns a cubed version of the input
+    :param1: Int
+    :return: Int
+    """
+    return num**3
+
+if __name__ == '__main__':
+    print("Cube Module")
---
+++ chapter_8_solution/main.py
@@ -0,0 +1,7 @@
+import cubed
+
+num = 5
+result = cubed.cube(5)
+
+print("Number: {}".format(num))
+print("Number Cubed: {}".format(result))
