05418f0e1f962087acf8635c3fc6737765f2f536
Pratham Doshi <pratham.doshi95@gmail.com>
2020-04-26 02:12:47 -0700

    update and answeres added

---
+++ chapter_8_solution.py
@@ -0,0 +1,21 @@
+# chapter 8 exercise solutions
+#--------------------------------------------------------------------------#
+
+# questions 1: Call a different function from statistic module
+print('\nQuestion 1 Solution: \n')
+
+#library with that helps run stats
+import statistics
+
+random_list = [1,2,3,5,7,4,5,6]
+mode = statistics.mode(random_list)
+print(random_list)
+print("Mode: {}".format(mode))
+print('*'*50)
+#--------------------------------------------------------------------------#
+  
+# questions 2: import module named cube and implement a function takes a 
+#              number as a parameter and returns a cube. Import the module and cube it.   
+print('Refer to chapter 8 Folder for the solution')  
+print('*'*50)
+#--------------------------------------------------------------------------#
