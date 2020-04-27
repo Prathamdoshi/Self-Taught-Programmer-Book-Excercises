c6ead2ba1d140446efa9b9696d802859bc790890
Pratham Doshi <pratham.doshi95@gmail.com>
2020-04-26 23:48:33 -0700

    hangman 80% done

--- chapter_10_solution/hangman.py
+++ chapter_10_solution/hangman.py
@@ -19,7 +19,17 @@ full_hangman = [["|","-","-","-","-"],
                 ["|"," "," ","O"," "],
                 ["|"," ","\\","|","/"],
                 ["|"," "," ","|"," "],
-                ["|"," ","/"," ","\\"]]            
+                ["|"," ","/"," ","\\"]]   
+
+#hangman list
+hangman_list = ["[1][3]",
+                "[2][3]",
+                "[3][2]",
+                "[3][3]",
+                "[3][4]",
+                "[4][3]",
+                "[5][2]",
+                "[5][4]"]         
 
 def hangman():
     for i in full_hangman:
@@ -27,9 +37,51 @@ def hangman():
             print(j,end=' ')
         print(' ')
 
-print('Hangman: ')
-hangman()
-
 # data
 #magic_word = w.generate_random_word()
 magic_word = 'prathamd' #word to be guessed
+magic_word_list = list(magic_word)
+
+# status variables
+number_of_chances = 8
+user_response_list = []
+
+print('Hangman: \n')
+hangman()
+
+game_start_string = "Press any key to start or 'q' to quit: "
+
+while input(game_start_string) != 'q': 
+
+   while number_of_chances > 0: 
+    
+    user_response = input("Guess the letter: ")
+
+    if user_response in magic_word: 
+        temp = magic_word.index(user_response)
+        string = "{}{}= '{}'".format("full_hangman",hangman_list[temp],user_response)
+        magic_word = magic_word.replace(user_response,'-',1)
+        user_response_list.append(user_response)
+        exec(string)
+        hangman()
+    
+        if sorted(user_response_list) == sorted(magic_word_list):
+            print('You won the game! Good job on guessing!!')
+            game_start_string = "Press any key to restart the game or 'q' to quit: "
+            break
+
+    elif user_response not in magic_word:
+        number_of_chances = number_of_chances - 1
+        string = "{}{}= '{}'".format("full_hangman",hangman_list[number_of_chances]," ")
+        exec(string)
+        hangman()
+
+
+    print("")
+
+        
+
+        
+        
+
+
