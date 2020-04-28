dbf67c65d01bb07a2851c9db819a8c92c717922e
Pratham Doshi <pratham.doshi95@gmail.com>
2020-04-28 12:07:40 -0700

    chapter 12 question 1 done

---
+++ chapter_11_solution.py
@@ -0,0 +1,36 @@
+# chapter 11 exercise solutions
+#--------------------------------------------------------------------------#
+
+# questions 1: Defina a class called Apple with four instance variable that represent four attributes of an apple
+print('\nQuestion 1 Solution: \n')
+class Apple():
+    def __init__(self,color, type_of_apple, weight, seadless):
+        self.color = color
+        self.type = type_of_apple
+        self.weight = weight
+        self.seadless = seadless
+
+apple_1 = Apple("Dark Red", "Opal", 10.0, True)
+print("Apple 1:")
+print("Color: {}".format(apple_1.color))
+print("Type: {}".format(apple_1.type))
+print("Weight: {}".format(apple_1.weight))
+print("Seadless: {}".format(apple_1.seadless))
+
+print('*'*50)
+#--------------------------------------------------------------------------#
+
+# questions 2: Create a circle, with a method area
+print('\nQuestion 2 Solution: \n')
+print('*'*50)
+#--------------------------------------------------------------------------#
+
+# questions 3: 
+print('\nQuestion 3 Solution: \n')
+print('*'*50)
+#--------------------------------------------------------------------------#
+
+# questions 4: 
+print('\nQuestion 4 Solution: \n')
+print('*'*50)
+#--------------------------------------------------------------------------#
