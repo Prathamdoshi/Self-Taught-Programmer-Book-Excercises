467a6b4203ba21c474ed7f96739eb810d432174b
Pratham Doshi <pratham.doshi95@gmail.com>
2020-05-04 00:33:53 -0700

    hangman.py done and tested

--- chapter_10_solution/hangman.py
+++ chapter_10_solution/hangman.py
@@ -23,43 +23,46 @@ hangman = [
           "|     / \     ",
           ]
 
+# variable to hold user's response to continue the game
 user_continue = True
 
 while user_continue:
 
-    key_word = w.generate_random_word()
+    key_word = w.generate_random_word() # use the module to get a word
+    key_word_list = list(key_word) # convert it to list to map it with the guessed word
+    wrong_choice_number = 0 # tracks the number of wrong responses 
+    guessed_word = [] # holds all the correct letters in a string
+    dash_str = len(key_word)*['_',] # dash representation of the word
+  
 
-    wrong_choice_number = 0
-    guessed_word = []
-    dash_str = ' _ '*len(key_word)
-
-    while wrong_choice_number < len(hangman):
+    while wrong_choice_number < len(hangman): 
         print("\nWord: ")
-        print(dash_str)
-        print("")
+        for dash in dash_str: print(dash,end=" ") # prints the dashes for the word
+        print("\n")
         guessed_letter = input("Guess the letter: ")
 
         if guessed_letter in key_word:
-            dash_str = dash_str.replace('_ ', guessed_letter, 1)
-            guessed_word.append(guessed_letter)
+            temp = key_word_list.index(guessed_letter) # get the index of the letter
+            dash_str[temp] = guessed_letter # change the dash with the right letter
+            key_word_list[temp]='_' # devalue the old letter so it handles repeats correctly
+            guessed_word.append(guessed_letter) # collect it for a further match
         
         if guessed_letter not in key_word:
-           for i in range(0,wrong_choice_number+1):
+           for i in range(0,wrong_choice_number+1): # prints the hangman till the stage user has corrected wrong
                print(hangman[i])
-           wrong_choice_number = wrong_choice_number + 1
+           wrong_choice_number = wrong_choice_number + 1 # increaces the penalty
 
-
-        if collections.Counter(sorted(guessed_word)) == collections.Counter(sorted(list(key_word))): 
-            print ("Congratulations!! You guessed the word '{}' correctly.".format(key_word)) 
+        if collections.Counter(sorted(guessed_word)) == collections.Counter(sorted(list(key_word))): # word match check
+            print ("\nCongratulations!! You guessed the word '{}' correctly.\n".format(key_word)) # win handler
             break;
         
-    if wrong_choice_number >=6:
-        print('\nSorry you loss.')
+    if wrong_choice_number >=6: # lost check
+        print('\nSorry you loss.') # losing handler
         
     
-    user_continue_response = input("Want to continue? 'y' for yes! 'n' for no: ")
+    user_continue_response = input("Want to continue? 'y' for yes! 'n' for no: ") # user continue check
     if user_continue_response == 'n':
-        print("Thanks for Playing!")
+        print("\nThanks for Playing!\n")
         break;
 
 
